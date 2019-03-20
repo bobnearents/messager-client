c3d0569bc620be2c568bed20408d698287b42f2b
bobnearents <bobnearents@yahoo.com>
2019-03-20 10:42:58 -0700

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  //API_ENDPOINT: "https://quiet-everglades-86567.herokuapp.com/api",
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: "https://quiet-everglades-86567.herokuapp.com/api",
+  //API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: "messager-client-auth-token"
 };
